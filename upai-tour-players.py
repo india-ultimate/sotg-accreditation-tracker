ac85da23ef837f1627e35751e5de16fa738105ee
Puneeth Chaganti <punchagan@muse-amuse.in>
2019-08-10 19:00:42 +0530

    Format upai-tour-players with black
    Co-authored-by: Ranjan Khyadad <ranjankhyadad@gmail.com>

--- upai-tour-players.py
+++ upai-tour-players.py
@@ -5,46 +5,47 @@ import json
 import os
 
 # Obtain this from https://upai.usetopscore.com/u/oauth-key
-CLIENT_ID = os.getenv('UPAI_CLIENT_ID')
-CLIENT_SECRET = os.getenv('UPAI_CLIENT_SECRET')
-BASE_URL = 'https://upai.usetopscore.com'
-assert CLIENT_ID, 'Need to set UPAI_CLIENT_ID envvar'
-assert CLIENT_SECRET, 'Need to set UPAI_CLIENT_SECRET envvar'
+CLIENT_ID = os.getenv("UPAI_CLIENT_ID")
+CLIENT_SECRET = os.getenv("UPAI_CLIENT_SECRET")
+BASE_URL = "https://upai.usetopscore.com"
+assert CLIENT_ID, "Need to set UPAI_CLIENT_ID envvar"
+assert CLIENT_SECRET, "Need to set UPAI_CLIENT_SECRET envvar"
 
 data = {
-    'grant_type': 'client_credentials',
-    'client_id': CLIENT_ID,
-    'client_secret': CLIENT_SECRET,
-
+    "grant_type": "client_credentials",
+    "client_id": CLIENT_ID,
+    "client_secret": CLIENT_SECRET,
 }
 
-response = requests.post('{}/api/oauth/server'.format(BASE_URL), data=data)
-ACCESS_TOKEN= response.json()['access_token']
-header = {'Authorization': 'Bearer {}'.format(ACCESS_TOKEN)}
+response = requests.post("{}/api/oauth/server".format(BASE_URL), data=data)
+ACCESS_TOKEN = response.json()["access_token"]
+header = {"Authorization": "Bearer {}".format(ACCESS_TOKEN)}
+
 
 def get_tournaments(year=None, header=None):
-    url = '{}{}?&per_page=100&order_by=date_desc'.format(
-        BASE_URL, '/api/events'
+    url = "{}{}?&per_page=100&order_by=date_desc".format(
+        BASE_URL, "/api/events"
     )
     r = requests.get(url, headers=header)
-    tournaments = r.json()['result']
+    tournaments = r.json()["result"]
     if year is not None:
         tournaments = [
-            t for t in tournaments if t['start'].startswith(str(year))
+            t for t in tournaments if t["start"].startswith(str(year))
         ]
     return tournaments
 
+
 def get_registrations(event_id=None, header=None):
-    url = '{}{}?event_id={}&fields[]=Person&fields[]=Team'.format(
-        BASE_URL, '/api/registrations', event_id
+    url = "{}{}?event_id={}&fields[]=Person&fields[]=Team".format(
+        BASE_URL, "/api/registrations", event_id
     )
     print(url)
     r = requests.get(url, headers=header)
-    registrations = r.json()['result']
+    registrations = r.json()["result"]
     return registrations
 
 
-if __name__ == '__main__':
-    registrations = get_registrations('140982', header)
+if __name__ == "__main__":
+    registrations = get_registrations("140982", header)
     for registration in registrations:
         print(registration)
