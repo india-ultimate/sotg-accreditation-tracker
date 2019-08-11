619ef950b71a597bc70e4b682daccb1d53c39b2c
Ranjan Khyadad <ranjankhyadad@gmail.com>
2019-08-11 17:56:50 +0530

    Add view to display event registrations
    The view currently displays all the registrations for an event,
    grouped by team name. A player can have multiple registrations, each
    for a different role. Such players are listed only once, along with
    all the roles.
    
    For making the development of the event_page view faster, currently
    the calls to get event_data are outside the event_page method, and
    hard-coded for a specific event. Once a caching strategy for this
    data is figured out, this code should be moved into the event_page
    method.
    
    Co-authored-by: Puneeth Chaganti <punchagan@muse-amuse.in>

--- tracker/urls.py
+++ tracker/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path(r"", views.home, name="home"),
     path(r"events", views.events, name="events"),
+    path(r"event/<int:event_id>", views.event_page, name="event_page"),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, HttpResponse
 
-from uc_api_helpers import get_tournaments
+from uc_api_helpers import get_registrations, get_tournaments
 
 # FIXME: Move into events view
 data = get_tournaments()
+# FIXME: Move into event_page view
+event_data = get_registrations(135604)
 
 
 def home(request):
@@ -13,3 +15,8 @@ def home(request):
 def events(request):
     context = {"events": data}
     return render(request, "tracker/event-list.html", context)
+
+
+def event_page(request, event_id):
+    context = {"registrations": event_data}
+    return render(request, "tracker/event-page.html", context)
