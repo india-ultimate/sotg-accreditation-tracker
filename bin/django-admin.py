06f2e7b6ae293619f7e0a11390bfa38eb8b1982a
vkrishnaswam <vivekkrishnaswamy88@gmail.com>
2019-08-17 17:50:37 +0530

    Procfile

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/mnt/c/Users/vkrishnaswam/Projects/sotg-accreditation-tracker/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
