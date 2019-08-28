360be035a5dea175a5c511b4baafbbc78015bb0f
Ranjan Khyadad <ranjankhyadad@gmail.com>
2019-08-28 16:07:44 +0530

    Create model for accreditation

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Accreditation(models.Model):
+    STANDARD = 'Standard'
+    ADVANCED = 'Advanced'
+    TYPE_CHOICES = [
+        (STANDARD, 'Standard'),
+        (ADVANCED, 'Advanced'),
+    ]
+    name = models.CharField(max_length=100, blank=False)
+    email = models.EmailField(blank=False)
+    type = models.CharField(choices=TYPE_CHOICES, default=STANDARD, max_length=8, blank=False)
+    date = models.DateField()
+    uc_username = models.CharField(max_length=50, blank=False)
+    wfdf_userid = models.IntegerField()
+    last_modified_at = models.DateTimeField(auto_now=True)
