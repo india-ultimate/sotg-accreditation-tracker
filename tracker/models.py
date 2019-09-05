7a7ebf9a732ce95d4ff454f2f55221e9d29af6aa
Ranjan Khyadad <ranjankhyadad@gmail.com>
2019-09-05 15:36:21 +0530

    Enforce unique constraint on email, uc_username and wfdf_userid

--- tracker/models.py
+++ tracker/models.py
@@ -9,9 +9,9 @@ class Accreditation(models.Model):
         (ADVANCED, 'Advanced'),
     ]
     name = models.CharField(max_length=100, blank=False)
-    email = models.EmailField(blank=False)
+    email = models.EmailField(blank=False, unique=True)
     type = models.CharField(choices=TYPE_CHOICES, default=STANDARD, max_length=8, blank=False)
     date = models.DateField(verbose_name="Accreditation Date", blank=True)
-    uc_username = models.CharField(max_length=50, blank=False, verbose_name='Ultimate Central username')
-    wfdf_userid = models.IntegerField(verbose_name='WFDF user ID', blank=True)
+    uc_username = models.CharField(max_length=50, blank=False, verbose_name='Ultimate Central username', unique=True)
+    wfdf_userid = models.IntegerField(verbose_name='WFDF user ID', blank=True, unique=True)
     last_modified_at = models.DateTimeField(auto_now=True)
