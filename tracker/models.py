2fa63ba2179c9744d98ff7f62515837f5884a097
Puneeth Chaganti <punchagan@muse-amuse.in>
2019-09-09 12:45:18 +0530

    Format using models.py using black

--- tracker/models.py
+++ tracker/models.py
@@ -3,7 +3,6 @@ from django.db import models
 from datetime import date
 
 
-
 class Accreditation(models.Model):
     STANDARD = "Standard"
     ADVANCED = "Advanced"
@@ -13,16 +12,28 @@ class Accreditation(models.Model):
     type = models.CharField(
         choices=TYPE_CHOICES, default=STANDARD, max_length=8, blank=False
     )
-    date = models.DateField(validators=[MaxValueValidator(limit_value=date.today(), message='Please enter valid date')],
-                            verbose_name="Accreditation Date")
+    date = models.DateField(
+        validators=[
+            MaxValueValidator(
+                limit_value=date.today(),
+                message="Please enter valid accreditation date",
+            )
+        ],
+        verbose_name="Accreditation Date",
+    )
     uc_username = models.CharField(
         max_length=50,
         blank=False,
         verbose_name="Ultimate Central username",
         unique=True,
     )
-    wfdf_userid = models.IntegerField(validators=[MinValueValidator(limit_value=0, message='Please enter valid ID')],
-                                      verbose_name="WFDF user ID", unique=True)
+    wfdf_userid = models.IntegerField(
+        validators=[
+            MinValueValidator(limit_value=0, message="Please enter valid ID")
+        ],
+        verbose_name="WFDF user ID",
+        unique=True,
+    )
     last_modified_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
