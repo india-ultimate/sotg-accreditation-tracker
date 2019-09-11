60b1109d215c0c051fc040f77c669103c82bdd75
Puneeth Chaganti <punchagan@muse-amuse.in>
2019-09-11 19:02:22 +0530

    Add a management command to update cached Ultimate Central data

---
+++ tracker/management/commands/update_uc_data.py
@@ -0,0 +1,32 @@
+import json
+
+import arrow
+from django.core.cache import cache
+from django.core.management.base import BaseCommand, CommandError
+
+from uc_api_helpers import get_registrations, get_tournaments
+
+
+class Command(BaseCommand):
+    help = "Update the caches with Ultimate Central data"
+
+    def handle(self, *args, **options):
+        print("Updating tournament data in cache")
+        tournaments = get_tournaments()
+        if tournaments:
+            cache.set("event-list", json.dumps(tournaments))
+        print("Saved tournament data to cache")
+
+        print("Fetching data for tournaments to cache...")
+        now = arrow.utcnow()
+        for tournament in json.loads(cache.get("event-list")):
+            end = arrow.get(tournament["end"])
+            if end < now:
+                continue
+            event_id = tournament["id"]
+            print("Caching event-{} registrations".format(event_id))
+            key = "event-registrations-{}".format(event_id)
+            registrations = get_registrations(event_id)
+            if registrations:
+                cache.set(key, json.dumps(registrations))
+            print("Cached event-{} registrations".format(event_id))
